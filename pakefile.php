b2092c98d38dd21fe869e63f21f6bb1b63e10047
gg <gg@c49d5ff4-bf5c-496b-a955-7cf9d1d6d221>
2011-04-04 08:54:13 +0000

    - add bootstrap capabilities

--- pakefile.php
+++ pakefile.php
@@ -1,20 +1,111 @@
 <?php
 /**
- * eZExtensionBuilder pakefile
- * Needs the Pake tool to run: https://github.com/indeyets/pake/wiki
- *
- * @author G. Giunta
- * @copyright
- * @license (C) G. Giunta 2011
- * @version SVN: $Id$
- *
- * @todo switch to an ini-file based configuration instead of yaml? advantages:
+* eZExtensionBuilder pakefile:
+* a script to build & package eZPublish extensions
+*
+* Needs the Pake tool to run: https://github.com/indeyets/pake/wiki
+* It can bootstrap, by downloading all required components from the web
+*
+* @author    G. Giunta
+* @copyright (C) G. Giunta 2011
+* @license
+* @version   SVN: $Id$
+*
+* @todo switch to an ini-file based configuration instead of yaml? advantages:
 *        . easier options parsing, everything is on 2 levels
 *        . pake code holds options instead of our class
+*
+* @todo move all known paths to class constants
+*
+* @todo add to php include dir a custom dir for our own pake tasks
 *
 * @bug at least on win, after using svn to checkout a project, the script does
 *      not have enough rights to remove the checkout dir...
 */
+
+// First off, test if user is running directly this script
+// (we allow both direct invokation via "php pakefile.php" and invocation via "php pake.php")
+if ( !function_exists( 'pake_desc' ) )
+{
+    // the folder where this script installs the pake tool is pake/src
+    if ( file_exists( 'pake/src/bin/pake.php' ) )
+    {
+        include( 'pake/src/bin/pake.php' );
+        $pake = pakeApp::get_instance();
+        $pake->run();
+    }
+    else
+    {
+
+        echo "Pake tool not found. Bootstrap needed\n  (automatic download of missing components from project.ez.no)\n";
+        do
+        {
+            echo 'Continue? [y/n] ';
+            $fp = fopen('php://stdin', 'r');
+            $ok = trim( strtolower( fgets( $fp ) ) );
+            fclose( $fp );
+            if ( $ok == 'y' )
+            {
+                break;
+            }
+            else if ( $ok == 'n' )
+            {
+                exit ( 0 );
+            }
+            echo "\n";
+        } while( true );
+
+        if ( is_file( 'pake ' ) )
+        {
+            echo "Error: could not create 'pake' directory to install the extension because a file named 'pake' exists";
+            exit( -1 );
+        }
+
+        if ( is_dir( 'pake') )
+        {
+            /// @todo test: if dir is not empty, ask for confirmation,
+            ///       least we overwrite something
+        }
+
+        if ( !is_dir( 'pake' ) && !mkdir( 'pake' ) )
+        {
+            echo "Error: could not create 'pake' directory to install the extension";
+            exit( -1 );
+        }
+
+        // download components
+        $src = 'http://svn.projects.ez.no/ezextensionbuilder/stable/pake/ezextensionbuilder_pakedir.zip';
+        $zipfile = tempnam( "tmp", "zip" );
+        if ( !file_put_contents( $zipfile, file_get_contents( $src ) ) )
+        {
+            echo "Error: could not download source file $src";
+            exit -1;
+        }
+
+        // unzip them
+        $zip = new ZipArchive;
+        if ( $zip->open( $zipfile ) !== true )
+        {
+            echo "Error: downloaded source file $src is not a valid zip file";
+            exit -1;
+        }
+        if ( !$zip->extractTo( 'pake' ) )
+        {
+            echo "Error: could not decompress source file $zipfile";
+            $zip->close();
+            exit -1;
+        }
+        $zip->close();
+        unlink( $zipfile );
+
+        echo
+            "Succesfully downloaded sources\n" .
+            "  Next steps: copy pake/options-sample.yaml to pake/options.yaml, edit it\n" .
+            "  then run again this script";
+        exit( 0 );
+
+    }
+}
 
 pake_desc( 'Shows help message' );
 pake_task( 'default' );
@@ -25,8 +116,8 @@ pake_task( 'show-properties' );
 pake_desc( 'Prepares the extension to be built' );
 pake_task( 'init' );
 
-//pake_desc( 'Builds the extension' );
-//pake_task( 'build', array( 'init' ) );
+pake_desc( 'Builds the extension' );
+pake_task( 'build', array( 'init' ) );
 
 pake_desc( 'Removes the entire build directory' );
 pake_task( 'clean' );
@@ -78,6 +169,11 @@ pake_task( 'create-package-tarballs' );
 pake_desc( 'Converts an existing ant properties file in its corresponding yaml version' );
 pake_task( 'convert-configuration' );
 
+// This file could be included twice, avoid redefining functions and classes
+
+if ( !function_exists( 'run_default' ) )
+{
+
 // ***
 
 function run_default()
@@ -92,6 +188,7 @@ function run_show_properties()
     pake_echo ( 'Extension name: ' . $opts['extension']['name'] );
 }
 
+/// @todo add a dependency on a check-updates task that updates script itself
 function run_init()
 {
     $opts = eZExtBuilder::getOpts();
@@ -119,15 +216,45 @@ function run_init()
     }
 
     // remove files
+
+    // known files/dirs not to be packed.
+    /// @todo !important shall we make this configurable?
+    $files = array( 'ant', 'build.xml', 'pake', 'pakefile.php' );
+    // files from user configuration
     if ( file_exists( 'pake/files.to.exclude.txt' ) )
     {
-        $files = file( 'pake/files.to.exclude.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES );
-        $files = pakeFinder::type( 'file' )->name( $files )->in( $destdir );
-        foreach ( $files as $file )
+        $files = array_merge( $files, file( 'pake/files.to.exclude.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES ) );
+    }
+    foreach ( $files as $i => $file )
+    {
+        $file = trim( $file );
+        if ( $file == '' || $file[0] == '#' )
         {
-            pake_remove( $file, '' );
+            unset( $files[$i] );
         }
     }
+
+    $files = pakeFinder::type( 'any' )->name( $files )->in( $destdir );
+    foreach ( $files as $file )
+    {
+        pake_remove( $file, '' );
+    }
+}
+
+function run_build()
+{
+    /// @todo shall we pass via some pakeApp call?
+    run_update_ezinfo();
+    run_update_license_headers();
+    run_update_extra_files();
+    run_generate_documentation();
+    run_generate_md5sums();
+    run_check_sql_files();
+    run_check_gnu_files();
+    //run_eznetwork_certify();
+    run_update_package_xml();
+    run_generate_ezpackage_xml_definition();
+    run_create_package_tarballs();
 }
 
 function run_clean()
@@ -138,7 +265,7 @@ function run_clean()
 
 function run_clean_all()
 {
-    /// @todo shall we pass via some pake call?
+    /// @todo shall we pass via some pakeApp call?
     run_clean();
     run_dist_clean();
 }
@@ -237,7 +364,7 @@ class eZExtBuilder
         return true;
     }
 
-    /// @todo move to a separate class
+    /// @todo move to a separate class to slim down base class
     static function covertPropertyFileToYamlFile( $infile, $outfile='pake/options.yaml', $transform = array(), $prepend='' )
     {
         $current = array();
@@ -309,6 +436,8 @@ class eZExtBuilder
         $ok = !file_exists( $outfile ) || ( pake_input( "Destionation file $outfile exists. Overwrite? [y/n]", 'n' ) == 'y' );
         $ok && file_put_contents( $outfile, $prepend . implode( $out, "\n" ) );
     }
+
+}
 
 }
 
